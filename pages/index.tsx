0eb5d058b2f47a569ee785db97145a79466320a9
Amar-Omerika <amar.omerika@edu.fit.ba>
2023-04-06 13:19:30 +0200

    Created heroBanner component, fetched data, make types for product interface

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,36 @@
 import React from "react";
+import { client } from "../lib/client";
+import { Product, FooterBanner, HeroBanner } from "@/components";
 
-const Home = () => {
+interface Props {
+	products?: any[];
+	bannerData?: any[];
+}
+const Home = ({ products, bannerData }: Props) => {
 	return (
-		<>
-			<div>
+		<div>
+			<HeroBanner heroBanner={bannerData?.length && bannerData[0]} />
+			<div className="products-heading">
 				<h2>Best Selling Products</h2>
-				<p>Speakerx of many variations</p>
+				<p>Speakers of many variations</p>
 			</div>
-			<div>{["product 1", "product 2"].map((product) => product)}</div>
-		</>
+			<div className="products-container">
+				{["product 1", "product 2"].map((product) => product)}
+			</div>
+			<FooterBanner />
+		</div>
 	);
 };
 
+export const getServerSideProps = async () => {
+	const query = '*[_type == "product"]';
+	const products = await client.fetch(query);
+
+	const bannerQuery = '*[_type == "banner"]';
+	const bannerData = await client.fetch(bannerQuery);
+
+	return {
+		props: { products, bannerData },
+	};
+};
 export default Home;
---
+++ types/products.ts
@@ -0,0 +1,14 @@
+export interface ProductDataInterface {
+	buttonText: string;
+	desc: string;
+	discount: string;
+	image: any;
+	largeText1: string;
+	largeText2: string;
+	midText: string;
+	product: string;
+	saleTime: string;
+	smallText: string;
+	_createdAt: any;
+	_id: string;
+}
