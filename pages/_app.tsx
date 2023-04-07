c84a357f65df85c47959eb880e124e0c6eb97b76
Amar-Omerika <amar.omerika@edu.fit.ba>
2023-04-07 14:59:31 +0200

    Babelrc update and various fix, typescript related

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
-import { Layout } from "@/components";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import React from "react";
+import { Toaster } from "react-hot-toast";
 
-export default function App({ Component, pageProps }: AppProps) {
+import { Layout } from "../components";
+import "../styles/globals.css";
+// import { StateContext } from '../context/StateContext';
+
+function MyApp({ Component, pageProps }) {
 	return (
+		// <StateContext>
 		<Layout>
+			<Toaster />
 			<Component {...pageProps} />
 		</Layout>
+		// </StateContext>
 	);
 }
+
+export default MyApp;
