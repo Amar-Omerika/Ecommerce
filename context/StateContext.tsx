03198b86058263c6d3bc42fe43ccd2ac229c4ac8
Amar-Omerika <amar.omerika@edu.fit.ba>
2023-07-18 16:02:30 +0200

    Installing packages for netlify  build and some code refactoring

--- context/StateContext.tsx
+++ context/StateContext.tsx
@@ -11,7 +11,6 @@ export const StateContext = ({ children }: any) => {
 	const [qty, setQty] = useState(1);
 	const [theme, setTheme] = useState("light"); // 'light' or 'dark' or any other themes you have
 
-	console.log("Theme", theme);
 	const increaseQty = () => {
 		setQty((prevQty) => prevQty + 1);
 	};
