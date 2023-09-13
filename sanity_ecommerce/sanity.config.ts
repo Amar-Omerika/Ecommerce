1dc7692a622406a266a744b40dced7b77906bd2a
Amar-Omerika <72925790+Amar-Omerika@users.noreply.github.com>
2023-09-13 14:34:52 +0200

    Trying to get rid of the plugins

--- sanity_ecommerce/sanity.config.ts
+++ sanity_ecommerce/sanity.config.ts
@@ -2,9 +2,7 @@ import {defineConfig, isDev} from 'sanity'
 import {visionTool} from '@sanity/vision'
 import {deskTool} from 'sanity/desk'
 import {schemaTypes} from './schemas'
-import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
 
-const devOnlyPlugins = [getStartedPlugin()]
 
 export default defineConfig({
   name: 'default',
@@ -13,7 +11,7 @@ export default defineConfig({
   projectId: '77imq2l1',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+  plugins: [deskTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
